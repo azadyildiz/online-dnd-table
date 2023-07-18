2ad8c0736c15ce139d751c09b7d64a25ae1eff66
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-07-18 04:01:19 +0300

    mongoose installed and connected to local db

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+async function dbConnection() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/online-dnd-table');
+        console.log('Connected to online-dnd-table db.');
+    } catch (err) {
+        console.log(err);
+    }
+}
+dbConnection();
+
 
 const app = express();
-
 const port = 5000;
 
 app.listen(port,()=>{
