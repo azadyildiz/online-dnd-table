300e1cd132cc00897200c7beed0b11d7e057857f
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-07-18 19:41:15 +0300

    static fields made dynamic with dotenv

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const authRoutes = require('./routes/authRoutes');
+const verifyToken = require('./middlewares/verifyToken');
+
+dotenv.config();
 
 async function dbConnection() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/online-dnd-table');
-        console.log('Connected to online-dnd-table db.');
+        await mongoose.connect(`${process.env.DB_HOST_AND_PORT}/${process.env.DB_NAME}`);
+        console.log('Connected to mongoDB.');
     } catch (err) {
         console.log(err);
     }
 }
 dbConnection();
 
-
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', authRoutes);
+
+
+
+app.listen(port, () => {
     console.log(`Server running on ${port} port.`);
-})
+});
