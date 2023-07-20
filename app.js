5bb04aefcd6713211dabea676a720eae6947b936
Azad YILDIZ <azad.yildiz119@gmail.com>
2023-07-20 06:56:02 +0300

    campaign routes added to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 const authRoutes = require('./routes/authRoutes');
+const campaignRoutes = require('./routes/campaignRoutes');
+
 const verifyToken = require('./middlewares/verifyToken');
 
 dotenv.config();
@@ -24,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoutes);
+app.use('/campaigns', verifyToken, campaignRoutes);
 
 
 
